df94a1af5a1c412ae63c7574aa7bd13a373a38da
Nishon Tandukar <nishon.tan@gmail.com>
2017-06-30 21:19:52 +0545

    removed lang files

--- app/src/main/java/com/naxa/nepal/sudurpaschimanchal/application/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.naxa.nepal.sudurpaschimanchal.application;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class BaseActivity extends AppCompatActivity {
-    public BaseActivity() {
-        LocaleUtils.updateConfig(this);
-    }
-}
