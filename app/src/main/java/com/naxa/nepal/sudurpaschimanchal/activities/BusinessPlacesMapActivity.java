d340c9a6088aab9e2fd231a7ce52609857f7426c
Samir Dangal <Samir Dangal>
2017-07-30 14:53:38 +0545

    map for business xml created

---
+++ app/src/main/java/com/naxa/nepal/sudurpaschimanchal/activities/BusinessPlacesMapActivity.java
@@ -0,0 +1,58 @@
+package com.naxa.nepal.sudurpaschimanchal.activities;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Spinner;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.naxa.nepal.sudurpaschimanchal.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BusinessPlacesMapActivity extends Activity {
+
+    @BindView(R.id.map)
+    MapView map;
+    @BindView(R.id.business_list_spinner)
+    Spinner businessListSpinner;
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_places_map);
+        ButterKnife.bind(this);
+    }
+
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+}
