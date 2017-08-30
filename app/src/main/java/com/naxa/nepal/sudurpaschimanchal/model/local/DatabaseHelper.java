4a4c3692274456f732774942b23a69c2ad33fb29
Nishon Tandukar <nishon.tan@gmail.com>
2017-08-30 17:32:02 +0545

    Chanaged database version

--- app/src/main/java/com/naxa/nepal/sudurpaschimanchal/model/local/DatabaseHelper.java
+++ app/src/main/java/com/naxa/nepal/sudurpaschimanchal/model/local/DatabaseHelper.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     private static final String DATABASE_NAME = "fwdc.db";
 
